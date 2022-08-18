0a9db55dcbafd388fe8998d14ce8f7364f4ca027
That IT Nerd <thatitguy@gmail.com>
2022-08-18 22:11:05 +0530

    side bar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,24 +1,49 @@
-import React from 'react'
- import styles from './sidebar.module.scss'
- import Tabs from '../../JSON/Tabs.json';
+import React from "react";
+import styles from "./sidebar.module.scss";
+import Tabs from "../../JSON/Tabs.json";
+import Image from "next/image";
+import Link from "next/link";
+import logoutIcon from "../../public/images/logout.png";
+import brandIcon from "../../public/images/brand.png";
+
 const Sidebar = () => {
-    return (
-        <div className={`basis-2/7 ${styles.sidebar}`}>
-            <div className={styles.title_block}>
-                <h1><span>Logo</span>Traders Block</h1>
-            </div>
-            
-                <ul className={styles.tabs_list}>
-                    {Tabs&& Tabs.map((tab)=>(
-                        <li key={tab.id}>{tab.name}</li>
-                    ))}
-                </ul>
+  return (
+    <div className={`basis-2/7 sidebar-wrapper  ${styles.sidebar}`}>
+      <div className={`${styles.title_block} `}>
+        <h1 className="flex flex-row ">
+          {" "}
+          <Image src={brandIcon} alt="" width={40} height={40} />
+          <p>Traders Block</p>
+        </h1>
+      </div>
 
-            <div className={styles.log_out}>
-                Log Out
-            </div>
-        </div>
-    )
-}
+      <ul className={styles.tabs_list}>
+        {Tabs &&
+          Tabs.map((tab, index) => (
+            <li
+              key={tab.id}
+              className={`flex flex-row ${index === 5 && "active-tab"} `}
+            >
+              <Link href="#">
+                <a>
+                  <Image src={tab.icon} alt="" width={14} height={14} />
+                  {tab.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+      </ul>
 
-export default Sidebar
+      <div className={styles.log_out}>
+        <Link href="#">
+          <a>
+            <Image src={logoutIcon} alt="" width={14} height={14} />
+            Log Out
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,26 @@
-import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import { useState } from 'react';
-import styles from '../styles/Home.module.css';
-import Sidebar from '../components/Sidebar/index';
-import HeroScreen from '../components/HeroScreen';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import styles from "../styles/Home.module.css";
+import Sidebar from "../components/Sidebar/index";
+import HeroScreen from "../components/HeroScreen";
 
 const Home: NextPage = () => {
-	const [toggle, setToggle] = useState<boolean>(true);
-	return (
-		<div
-			className={`container container-fluid flex flex-row ${
-				toggle ? 'dark' : 'light'
-			}`}
-		>
-			<Sidebar />
-			<HeroScreen />
-			<div
-				onClick={() => {
-					setToggle(!toggle);
-				}}
-			>
-				Toggle
-			</div>
-		</div>
-	);
+  const [toggle, setToggle] = useState<boolean>(true);
+  return (
+    <div className={`main-wrapper flex flex-row ${toggle ? "dark" : "light"}`}>
+      <Sidebar />
+      <HeroScreen />
+      <div
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+      >
+        Toggle
+      </div>
+    </div>
+  );
 };
 
 export default Home;
